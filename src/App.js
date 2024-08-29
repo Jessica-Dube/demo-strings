c6cb69edd104bad7cd33f822da5b78f3669ee912
Jessica Dubé <2318757@cstj.qc.ca>
2024-08-29 10:32:49 -0400

    done

--- src/App.js
+++ src/App.js
@@ -23,41 +23,53 @@ export default class App {
 		console.log(str1 + " et " + str2);
 
 		// En utilisant str1, créer la variable num1 qui vaut 9 puis la faire afficher
-		var num = str1.length;
-		console.log(num);
+		var num1 = str1.length;
+		console.log(num1);
 
 		// Faire afficher str2
 		console.log(str2);
 
 		// Faire afficher les 9 premiers caractères de str2
-		console.log(str2.slice(0, num));
+		console.log(str2.slice(0, num1));
 
 		// Faire afficher les 9 derniers caractères de str2
-		console.log(str2.slice (-num));
+		console.log(str2.slice(-num1));
 
 		// Créer la variable num2 qui a comme valeur la position du mot "rouge" (faire afficher)
 		var num2 = str2.indexOf("rouge");
 		console.log(num2);
 
 		// Faire afficher "Le petit chaperon bleu"
+		console.log(str2.slice(0, num2) + "bleu");
 
 		// Faire afficher "18/9=2"
 
+		console.log(num2 + "/" + num1 + "=" + (num2 / num1));
+
 		// Créer la variable num3 avec comme valeur la position de l'espace dans str1 (faire afficher)
+		var num3 = str1.indexOf(" ");
+		console.log(num3);
 
 		// Faire afficher "Raté"
+		console.log(str1.slice(num3 + 1));
 
 		// Faire afficher "Rita"
+		console.log(str1.slice(0, num3));
 
 		// Faire afficher "Raté, Rita"
+		console.log(str1.slice(num3 + 1) + ", " + str1.slice(0, num3));
 
 		// Faire afficher "LE PETIT CHAPERON ROUGE"
+		console.log(str2.toUpperCase());
 
 		// Faire afficher "L"
+		console.log(str2.toUpperCase().slice(0,1));
 
 		// Faire afficher "e petit chaperon rouge"
+		console.log(str2.slice(1));
 
 		// Faire afficher "Le petit chaperon rouge"
+		console.log(str2.toUpperCase().slice(0,1) + str2.slice(1));
 
 	}
 	/**
